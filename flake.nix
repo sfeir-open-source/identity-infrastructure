73edbbc4f00c2d2ee026350e056cd99cd45878d0
Shikanime Deva <deva.shikanime@protonmail.com>
2022-04-13 13:09:47 +0200

    Add Nix development environment config

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Application architecture reference for identity management";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+
+  outputs = { self, nixpkgs, ... }: {
+    devShell = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix
+      (system:
+        import ./shell.nix {
+          pkgs = import nixpkgs { inherit system; };
+        }
+      );
+    devShells = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix
+      (system:
+        {
+          identity-foundation-account = import ./identity-foundation-account/shell.nix {
+            pkgs = import nixpkgs { inherit system; };
+          };
+          identity-foundation-app = import ./identity-foundation-app/shell.nix {
+            pkgs = import nixpkgs { inherit system; };
+          };
+        });
+  };
+}
