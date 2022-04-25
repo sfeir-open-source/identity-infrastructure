47806579effd9521b16831d932b8ab91d2e49cf5
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-04-25 13:28:14 +0200

    Fix relative local file

--- identity-foundation-infrastructure/file.tf
+++ identity-foundation-infrastructure/file.tf
@@ -1,5 +1,5 @@
 resource "local_sensitive_file" "jwks" {
-  filename = "id_token.jwks.json"
+  filename = "${path.module}/id_token.jwks.json"
   content = jsonencode({
     keys = jsondecode(data.google_kms_secret.jwks_keys.plaintext)
   })
--- identity-foundation-infrastructure/kms.tf
+++ identity-foundation-infrastructure/kms.tf
@@ -1,11 +1,12 @@
 data "google_kms_key_ring" "terraform" {
+  project  = var.google_project
   name     = "terraform"
   location = var.google_region
 }
 
 data "google_kms_crypto_key" "sensible" {
-  name            = "sensible"
-  key_ring        = data.google_kms_key_ring.terraform.id
+  name     = "sensible"
+  key_ring = data.google_kms_key_ring.terraform.id
 }
 
 data "google_kms_secret" "jwks_keys" {
