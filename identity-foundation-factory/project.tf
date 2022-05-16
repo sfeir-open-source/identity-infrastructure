eb7d7afa6ee3ec375130c2d415a2f2489073f1e1
Shikanime Deva <deva.shikanime@protonmail.com>
2022-05-16 19:15:24 +0200

    Remove disable dependant APIS

--- identity-foundation-factory/project.tf
+++ identity-foundation-factory/project.tf
@@ -1,28 +1,28 @@
 resource "google_project_service" "artifact_registry" {
   project                    = var.google_project
   service                    = "artifactregistry.googleapis.com"
-  disable_dependent_services = true
+  disable_dependent_services = false
   disable_on_destroy         = true
 }
 
 resource "google_project_service" "cloud_build" {
   project                    = var.google_project
   service                    = "cloudbuild.googleapis.com"
-  disable_dependent_services = true
+  disable_dependent_services = false
   disable_on_destroy         = true
 }
 
 resource "google_project_service" "kms" {
   project                    = var.google_project
   service                    = "cloudkms.googleapis.com"
-  disable_dependent_services = true
+  disable_dependent_services = false
   disable_on_destroy         = true
 }
 
 resource "google_project_service" "secret_manager" {
   project                    = var.google_project
   service                    = "secretmanager.googleapis.com"
-  disable_dependent_services = true
+  disable_dependent_services = false
   disable_on_destroy         = true
 }
 
